bcd858e13d17f409996e664f8b8491260fd179a6
thegoose51 <cjm.wiki@gmail.com>
2020-07-09 23:58:51 -0400

    idk maybe this time?

--- precache-manifest.35687b2b22924d515f9ca12f9ce53f30.js
+++ precache-manifest.35687b2b22924d515f9ca12f9ce53f30.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "6f636c7428ad8edc1b8bdc2659bc19e1",
-    "url": "index.html"
+    "url": "/bdayview/index.html"
   },
   {
     "revision": "ced449c2a83db1aa8d31",
-    "url": "static/css/main.5d92b033.chunk.css"
+    "url": "/bdayview/static/css/main.5d92b033.chunk.css"
   },
   {
     "revision": "2c6906dffa0070bec939",
-    "url": "static/js/2.e79c42d9.chunk.js"
+    "url": "/bdayview/static/js/2.e79c42d9.chunk.js"
   },
   {
     "revision": "ced449c2a83db1aa8d31",
-    "url": "static/js/main.0d6e0222.chunk.js"
+    "url": "/bdayview/static/js/main.0d6e0222.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "static/js/runtime~main.a8a9905a.js"
+    "url": "/bdayview/static/js/runtime~main.a8a9905a.js"
   }
 ]);
