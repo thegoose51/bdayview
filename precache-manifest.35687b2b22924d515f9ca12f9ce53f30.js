938b24203874f8e91b1afcd13048a0d0ca742bda
thegoose51 <cjm.wiki@gmail.com>
2020-07-09 23:51:08 -0400

    view

---
+++ precache-manifest.35687b2b22924d515f9ca12f9ce53f30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f636c7428ad8edc1b8bdc2659bc19e1",
+    "url": "/bday/index.html"
+  },
+  {
+    "revision": "ced449c2a83db1aa8d31",
+    "url": "/bday/static/css/main.5d92b033.chunk.css"
+  },
+  {
+    "revision": "2c6906dffa0070bec939",
+    "url": "/bday/static/js/2.e79c42d9.chunk.js"
+  },
+  {
+    "revision": "ced449c2a83db1aa8d31",
+    "url": "/bday/static/js/main.0d6e0222.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/bday/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bday/precache-manifest.35687b2b22924d515f9ca12f9ce53f30.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bday/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
